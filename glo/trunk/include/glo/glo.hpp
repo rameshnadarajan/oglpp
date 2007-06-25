847df386f3de36f2b05dc1b09e7d5a404e8ed017
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2007-06-25 15:19:49 +0000

    ~ Adds support of gcc 4.x visibility.

--- glo/trunk/include/glo/glo.hpp
+++ glo/trunk/include/glo/glo.hpp
@@ -25,15 +25,28 @@
 
 #ifdef _WIN32
 
-   #ifdef GLO_EXPORTS
-   #define GLO_API	__declspec(dllexport)
-   #else
-   #define GLO_API	__declspec(dllimport)
-   #endif
+	#ifdef GLO_EXPORTS
+	#define GLO_API	__declspec(dllexport)
+	#else
+	#define GLO_API	__declspec(dllimport)
+	#endif
+
+	#define GLO_CLASS_API
+
+#elif defined(__GNUC__) && (__GNUC__>=4) && defined(__USE_DYLIB_VISIBILITY__)
+
+	#ifdef GLO_EXPORTS
+	#define GLO_API __attribute__ ((visibility("default")))
+	#define GLO_CLASS_API __attribute__ ((visibility("default")))
+	#else
+	#define GLO_API __attribute__ ((visibility("hidden")))
+	#define GLO_CLASS_API __attribute__ ((visibility("hidden")))
+	#endif
 
 #else
 
-    #define GLO_API
+	#define GLO_API
+	#define GLO_CLASS_API
 
 #endif
 
