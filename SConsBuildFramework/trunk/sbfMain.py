32e3594cab85a58a89f46ff6edfae6b5031ad3e8
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2008-03-26 15:36:41 +0000

    ~ Improves sbf robustness by using SCons exceptions (instead of simple output messages). + Adds support of cairomm1-2-4, cairo1-2-6 and itk3-4-0 in 'uses' option. cairo and cairomm uses components installed by gtkmm and gtk installer (no more version package in localExt directory). - Removes support of wxWidgets version 2-4 and 2-6 in 'uses' option, but updates support of wxWidgets2-8 on posix platforms.

--- SConsBuildFramework/trunk/sbfMain.py
+++ SConsBuildFramework/trunk/sbfMain.py
@@ -114,6 +114,8 @@ import string
 import sys
 
 from SCons.Script.SConscript import SConsEnvironment
+import SCons.Errors
+
 from sbfFiles import *
 
 
@@ -144,15 +146,16 @@ def getPathFromEnv( varname, normalizedPathname = True ) :
 
 ###### Options : validators and converters ######
 OptionUses_allowedValues = [	### @todo allow special values like '', 'none', 'all'
-					'boost1-33-1', 'cairo1-2-6', 'gtkmm2-4', 'itk2-6', 'ode', 'opengl', 'openil', 'openilu',
-					'glu', 'glut', 'sdl', 'sofa', 'wx2-6', 'wxgl2-6', 'wx2-8', 'wxgl2-8' ]
-								# cg|cgFX|imageMagick6|imageMagick++6
+					'boost1-33-1', 'cairo1-2-6', 'cairomm1-2-4', 'glu', 'glut', 'gtkmm2-4', 'itk3-4-0', 'ode', 'opengl',
+					'openil', 'openilu', 'sdl', 'sofa', 'wx2-8', 'wxgl2-8' ]
+					# cg|cgFX|imageMagick6|imageMagick++6
 
 OptionUses_alias = {
 		'boost'		: 'boost1-33-1',
 		'cairo'		: 'cairo1-2-6',
+		'cairomm'	: 'cairomm1-2-4',
 		'gtkmm'		: 'gtkmm2-4',
-		'itk'		: 'itk2-6',
+		'itk'		: 'itk3-4-0',
 		'wx'		: 'wx2-8',
 		'wxgl'		: 'wxgl2-8' }
 
@@ -168,7 +171,6 @@ def usesValidator( key, val, env ) :
 #			invalid_values += ' ' + value
 #=======================================================================================================================
 	if len(invalid_values) > 0 :
-		import SCons.Errors
 		raise SCons.Errors.UserError("Invalid value(s) for option uses:%s" % invalid_values)
 
 ### Splits the given string to obtain a list of values ###
@@ -1109,18 +1111,67 @@ Documentation on the options:
 			# print 'sbfWarning: uses=[\'%s\'] not supported on platform %s.' % ( elt, self.myPlatform )
 
 	def use_cairo( self, lenv, elt ) :
-		lenv.AppendUnique( CPPPATH = os.path.join( self.myIncludesInstallExtPaths[0], 'cairo' ) )
-		lenv.AppendUnique( LIBS = ['cairo', 'fontconfig', 'freetype', 'png', 'z' ] )
+		# Retrieves GTK_BASEPATH
+		gtkBasePath = getPathFromEnv('GTK_BASEPATH')
+		if gtkBasePath is None :
+			raise SCons.Errors.UserError("Unable to configure '%s'." % elt)
 
-	# TODO: GTK_BASEPATH and GTKMM_BASEPATH documentation, package gtkmm
+		# Sets CPPPATH
+		gtkCppPath = [ 'include/cairo', 'include' ]
+
+		for cppPath in gtkCppPath :
+			lenv.AppendUnique( CPPPATH = os.path.join(gtkBasePath, cppPath) )
+
+		# Sets LIBS, LIBPATH and CPPFLAGS
+		if self.myPlatform == 'win32' :
+			lenv.AppendUnique( LIBS = [	'cairo' ] )
+			lenv.AppendUnique( LIBPATH = [ os.path.join(gtkBasePath, 'lib') ] )
+
+			lenv.AppendUnique( CPPFLAGS = [ '/vd2', '/wd4250' ] )
+		else :
+			raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
+
+#=======================================================================================================================
+# for cairo package
+#		lenv.AppendUnique( CPPPATH = os.path.join( self.myIncludesInstallExtPaths[0], 'cairo' ) )
+#		lenv.AppendUnique( LIBS = ['cairo', 'fontconfig', 'freetype', 'png', 'z' ] )
+#=======================================================================================================================
+
+	def use_cairomm( self, lenv, elt ) :
+		# Configures cairo
+		self.use_cairo( lenv, elt )
+
+		# Retrieves GTKMM_BASEPATH
+		gtkmmBasePath	= getPathFromEnv('GTKMM_BASEPATH')
+		if gtkmmBasePath is None :
+			raise SCons.Errors.UserError("Unable to configure '%s'." % elt)
+
+		# Sets CPPPATH
+		gtkmmCppPath = ['include/cairomm-1.0']
+
+		for cppPath in gtkmmCppPath :
+			lenv.AppendUnique( CPPPATH = os.path.join(gtkmmBasePath, cppPath) )
+
+		# Sets LIBS and LIBPATH
+		if self.myPlatform == 'win32' :
+			if self.myConfig == 'release' :
+				lenv.AppendUnique( LIBS = [	'cairomm-1.0' ] )
+			else:
+				lenv.AppendUnique( LIBS = [	'cairomm-1.0d' ] )
+
+			lenv.AppendUnique( LIBPATH = [ os.path.join(gtkmmBasePath, 'lib') ] )
+		else :
+			raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
+
+	# TODO: GTK_BASEPATH and GTKMM_BASEPATH documentation, package gtkmm ?
 	def use_gtkmm( self, lenv, elt ) :
 		# Retrieves GTK_BASEPATH and GTKMM_BASEPATH
 		gtkBasePath		= getPathFromEnv('GTK_BASEPATH')
 		gtkmmBasePath	= getPathFromEnv('GTKMM_BASEPATH')
 		if	(gtkBasePath is None) or (gtkmmBasePath is None ) :
-			print 'sbfWarning: Unable to configure %s.' % elt
-			return
-		#
+			raise SCons.Errors.UserError("Unable to configure '%s'." % elt)
+
+		# Sets CPPPATH
 		gtkmmCppPath = ['include/libglademm-2.4', 'lib/libglademm-2.4/include', 'lib/gtkmm-2.4/include',
 						'include/gtkmm-2.4', 'lib/gdkmm-2.4/include', 'include/gdkmm-2.4', 'include/pangomm-1.4',
 						'include/atkmm-1.6', 'lib/libxml++-2.6/include', 'include/libxml++-2.6',
@@ -1137,7 +1188,7 @@ Documentation on the options:
 		for cppPath in gtkCppPath :
 			lenv.AppendUnique( CPPPATH = os.path.join(gtkBasePath, cppPath) )
 
-		#
+		# Sets LIBS, LIBPATH and CPPFLAGS
 		if self.myPlatform == 'win32' :
 #			lenv.AppendUnique( LIBS = [	'glademm-2.4', 'xml++-2.6', 'gtkmm-2.4', 'glade-2.0', 'gdkmm-2.4', 'atkmm-1.6',
 #										'pangomm-1.4', 'glibmm-2.4', 'cairomm-1.0', 'sigc-2.0',
@@ -1164,42 +1215,28 @@ Documentation on the options:
 
 			lenv.AppendUnique( CPPFLAGS = [ '/vd2', '/wd4250' ] )
 		else :
-			print 'sbfWarning: uses=[\'%s\'] not supported on platform %s.' % ( elt, self.myPlatform )
+			raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
 
 	def use_itk( self, lenv, elt ) :
-		# already and always done by sbf ' -DNOMINMAX'
+		# Already and always done by sbf on windows platform '/DNOMINMAX'
 
 		# includes
-		itkIncludes = []
-		if ( elt in [ 'itk2-6', 'itk' ] ) :
-			itkIncludes = [	'itk', 'itk/Algorithms', 'itk/BasicFilters', 'itk/Common', 'itk/expat', 'itk/Numerics', 'itk/IO',
-							'itk/Numerics/FEM', 'itk/Numerics/Statistics', 'itk/Numerics/NeuralNetworks', 'itk/SpatialObject',
-							'itk/Utilities/MetaIO', 'itk/Utilities/NrrdIO', 'itk/Utilities/DICOMParser', 'itk/Utilities',
-							'itk/Utilities/vxl/vcl', 'itk/Utilities/vxl/core' ]
-		else :
-			print "sbfWarning: ", elt, " unsupported."
+		itkIncludes = [	'itk', 'itk/Algorithms', 'itk/BasicFilters', 'itk/Common', 'itk/expat', 'itk/gdcm/src',
+						'itk/IO', 'itk/Numerics', 'itk/Numerics/FEM', 'itk/Numerics/NeuralNetworks', 'itk/Numerics/Statistics',
+						'itk/SpatialObject', 'itk/Utilities', 'itk/Utilities/MetaIO', 'itk/Utilities/NrrdIO',
+						'itk/Utilities/vxl/core', 'itk/Utilities/vxl/vcl' ]
 
 		for cppPath in self.myIncludesInstallExtPaths :
 			for include in itkIncludes :
 				lenv.Append( CPPPATH = os.path.join(cppPath, include) )
 
 		# libs
-		if ( self.myConfig == 'release' ) :
-			itkLibs = [	'ITKAlgorithms', 'ITKNumerics', 'ITKCommon', 'itksys', 'ITKStatistics', 'itkvnl_inst',
-						'itkvnl', 'itknetlib', 'itkvcl', 'itkvnl_algo', 'ITKStatistics', 'ITKFEM', 'ITKBasicFilters',
-						'ITKIO', 'itkjpeg8', 'itkjpeg16', 'itkpng', 'itktiff', 'ITKMetaIO', 'ITKEXPAT', 'itkzlib',
-						'ITKNrrdIO', 'itkgdcm', 'ITKDICOMParser', 'itkjpeg12', 'itkopenjpeg', 'ITKniftiio',
-						'ITKSpatialObject', 'ITKznz' ]
-		else :
-			itkLibs = [	'ITKAlgorithms_D', 'ITKNumerics_D', 'ITKCommon_D', 'itksys_D', 'ITKStatistics_D', 'itkvnl_inst_D',
-						'itkvnl_D', 'itknetlib_D', 'itkvcl_D', 'itkvnl_algo_D', 'ITKStatistics_D', 'ITKFEM_D', 'ITKBasicFilters_D',
-						'ITKIO_D', 'itkjpeg8_D', 'itkjpeg16_D', 'itkpng_D', 'itktiff_D', 'ITKMetaIO_D', 'ITKEXPAT_D', 'itkzlib_D',
-						'ITKNrrdIO_D', 'itkgdcm_D', 'ITKDICOMParser_D', 'itkjpeg12_D', 'itkopenjpeg_D', 'ITKniftiio_D',
-						'ITKSpatialObject_D', 'ITKznz_D' ]
+		itkLibs = [	'ITKAlgorithms', 'ITKBasicFilters', 'ITKCommon', 'ITKDICOMParser', 'ITKEXPAT', 'ITKFEM', 'itkgdcm',
+					'ITKIO', 'itkjpeg8', 'itkjpeg12', 'itkjpeg16', 'ITKMetaIO', 'ITKniftiio', 'ITKNrrdIO', 'ITKNumerics',
+					'itkopenjpeg', 'itkpng', 'ITKSpatialObject', 'ITKStatistics', 'itksys', 'itktiff', 'itkv3p_netlib',
+					'itkvcl', 'itkvnl', 'itkvnl_algo', 'itkvnl_inst', 'itkzlib', 'ITKznz' ]
 
 		lenv.Append( LIBS = itkLibs )
-		# 'snmpapi','wsock32'
-		# 'kernel32.lib', 'user32.lib', 'gdi32.lib', 'winspool.lib', 'comdlg32.lib', 'advapi32.lib', 'shell32.lib', 'ole32.lib', 'oleaut32.lib', 'uuid.lib', 'odbc32.lib', 'odbccp32.lib'
 
 	def use_openIL( self, lenv, elt ) :
 		if ( self.myPlatform == 'win32' ) :
@@ -1237,8 +1274,7 @@ Documentation on the options:
 		# Retrieves SOFA_PATH
 		sofa_path = getPathFromEnv('SOFA_PATH')
 		if sofa_path is None :
-			print 'sbfWarning: Unable to configure %s.' % elt
-			return
+			raise SCons.Errors.UserError("Unable to configure '%s'." % elt)
 
 		#
 		lenv['CPPPATH'] += [ os.path.join(sofa_path, 'modules') ]
@@ -1253,7 +1289,7 @@ Documentation on the options:
 				lenv['LIBPATH']	+= [ os.path.join( sofa_path, 'lib/win32/DebugVC8' ) ]
 			lenv['LIBPATH'] += [ os.path.join( sofa_path, 'lib/win32/Common' ) ]
 		else :
-			print 'sbfWarning: uses=[\'%s\'] not supported on platform %s.' % ( elt, self.myPlatform )
+			raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
 
 	#@todo Adds support to both ANSI and Unicode version of wx
 	#@todo Adds support static/dynamic and db stuff (see http://www.wxwidgets.org/wiki/index.php/MSVC_.NET_Setup_Guide)
@@ -1262,20 +1298,10 @@ Documentation on the options:
 
 			lenv.Append( CPPDEFINES = [ 'WXUSINGDLL', '__WIN95__' ] )
 
-			if self.myType == 'exec' :
-				lenv.Replace( LINKFLAGS = ['/SUBSYSTEM:WINDOWS'] )
+			if self.myType == 'exec' and (not elt.startswith('wxgl')):
+				lenv.Append( LINKFLAGS = '/SUBSYSTEM:WINDOWS' )
 
-			if elt == 'wx2-6' :
-				if self.myConfig == 'release' :
-					lenv.Append( LIBS = [	'wxbase26', 'wxmsw26_adv', 'wxmsw26_core', 'wxmsw26_html', 'wxbase26_net', 'wxmsw26_xrc' ] )		### xml
-				else :
-					lenv.Append( LIBS = [	'wxbase26d', 'wxmsw26d_adv', 'wxmsw26d_core', 'wxmsw26d_html', 'wxbase26d_net', 'wxmsw26d_xrc' ] )	### xml
-			elif elt == 'wxgl2-6' :
-				if self.myConfig == 'release' :
-					lenv.Append( LIBS = ['wxmsw26_gl'] )
-				else :
-					lenv.Append( LIBS = ['wxmsw26d_gl'] )
-			elif elt in ['wx2-8', 'wx'] :
+			if elt == 'wx2-8' :
 				if self.myConfig == 'release' :
 					lenv.Append( LIBS = [	'wxbase28', 'wxbase28_net', 'wxbase28_xml', 'wxmsw28_adv', 'wxmsw28_aui', 'wxmsw28_core',
 											'wxmsw28_html', 'wxmsw28_media', 'wxmsw28_qa', 'wxmsw28_richtext', 'wxmsw28_xrc'	] )
@@ -1284,104 +1310,104 @@ Documentation on the options:
 					lenv.Append( LIBS = [	'wxbase28d', 'wxbase28d_net', 'wxbase28d_xml', 'wxmsw28d_adv', 'wxmsw28d_aui', 'wxmsw28d_core',
 											'wxmsw28d_html', 'wxmsw28d_media', 'wxmsw28d_qa', 'wxmsw28d_richtext', 'wxmsw28d_xrc'	] )
 										# wxbase28d_odbc, wxmsw28d_dbgrid
-			elif elt in ['wxgl2-8', 'wxgl'] :
+			elif elt == 'wxgl2-8' :
 				if self.myConfig == 'release' :
 					lenv.Append( LIBS = [	'wxmsw28_gl'	] )
 				else :
 					lenv.Append( LIBS = [	'wxmsw28d_gl'	] )
 			else :
-				print "sbfWarning: Unsupported version of wxWidgets"
+				raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
 		elif self.myPlatform == 'darwin' :
-			print "sbfWarning: Unsupported version of wxWidgets"
+			raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
 		else :
-			#lenv['CXXFLAGS'] += ' -DGTK_NO_CHECK_CASTS -D__WXGTK__ -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES '					### FIXME: bug in ParseConfig() ? remove me ?
-			if elt == 'wx2-4' :
-				env.ParseConfig('wx-config --prefix=' + os.path.join( self.myInstallExtPaths[0], 'lib' ) + ' --cppflags --ldflags --libs')
-			elif elt == 'wxgl2-4' :
-				env.ParseConfig('wx-config --prefix=' + os.path.join( self.myInstallExtPaths[0], 'lib' ) + ' --cppflags --ldflags --libs --gl-libs')
-			elif elt in ['wx2-6', 'wx'] :
+			if elt == 'wx2-8' :
 				if self.myConfig == 'release' :
-					env.ParseConfig('wx-config --debug=no --cppflags --libs base adv core html net xml xrc')
+					env.ParseConfig('wx-config-gtk2-unicode-release-2.8 --cppflags --libs')
 				else:
-					env.ParseConfig('wx-config --debug=yes --cppflags --libs base adv core html net xml xrc')
-			elif elt == 'wxgl2-6' :
+					env.ParseConfig('wx-config-gtk2-unicode-debug-2.8 --cppflags --libs')
+			elif elt == 'wxgl2-8' :
 				if self.myConfig == 'release' :
-					env.ParseConfig('wx-config --debug=no --libs gl')
+					env.ParseConfig('wx-config-gtk2-unicode-release-2.8 --gl-libs')
 				else:
-					env.ParseConfig('wx-config --debug=yes --libs gl')
-
+					env.ParseConfig('wx-config-gtk2-unicode-debug-2.8 --gl-libs')
+			else :
+				raise SCons.Errors.UserError("Uses=[\'%s\'] not supported on platform %s." % (elt, self.myPlatform) )
 
 	def uses( self, lenv ) :
 
 		for elt in lenv['uses'] :
+			# Converts to lower case
 			elt = string.lower( elt )
 
 			### configure boost ###
-			if ( elt in ['boost', 'boost1-33-1'] ) :
+			if elt == 'boost1-33-1' :
 				self.use_boost( lenv, elt )
 
 			### configure cairo ###
-			elif ( elt in ['cairo', 'cairo1-2-6' ] ) :
+			elif elt == 'cairo1-2-6' :
 				self.use_cairo( lenv, elt )
 
-			### configure gtk/gtkmm ###
-			elif ( elt in ['gtkmm', 'gtkmm2-4'] ) :
-				self.use_gtkmm( lenv, elt );
-
-			### configure itk ###
-			elif ( elt in ['itk', 'itk2-6'] ) :
-				self.use_itk( lenv, elt )
-
-			### configure ODE ###
-			elif ( elt == 'ode' ) :
-				lenv['LIBS'] += ['ode']
-
-			### configure OpenGL ###
-			elif ( elt == 'opengl' ) :
-				if ( self.myPlatform == 'win32' ) :
-					lenv['LIBS'] += ['opengl32']
-				else :
-					lenv['LIBS'] += ['GL']
-
-			### configure openIL ###
-			elif ( elt == 'openil' ) :
-				self.use_openIL( lenv, elt )
-
-			### configure openILU ###
-			elif ( elt == 'openilu' ) :
-				self.use_openILU( lenv, elt )
+			### configure cairomm ###
+			elif elt == 'cairomm1-2-4' :
+				self.use_cairomm( lenv, elt )
 
 			### configure glu ###
-			elif ( elt == 'glu' ) :
-				if ( self.myPlatform == 'win32' ) :
+			elif elt == 'glu' :
+				if self.myPlatform == 'win32' :
 					lenv['LIBS'] += ['glu32']
 				else :
 					lenv['LIBS'] += ['GLU']
 
 			### configure glut ###
-			elif ( elt == 'glut' ) :
-				if ( self.myPlatform == 'win32' ) :
+			elif elt == 'glut' :
+				if self.myPlatform == 'win32' :
 					lenv['LIBS'] += ['glut32']
 				else :
 					lenv['LIBS'] += ['glut']
 
+			### configure gtk/gtkmm ###
+			elif elt == 'gtkmm2-4' :
+				self.use_gtkmm( lenv, elt );
+
+			### configure itk ###
+			elif elt == 'itk3-4-0' :
+				self.use_itk( lenv, elt )
+
+			### configure ODE ###
+			elif elt == 'ode' :
+				lenv['LIBS'] += ['ode']
+
+			### configure OpenGL ###
+			elif elt == 'opengl' :
+				if self.myPlatform == 'win32' :
+					lenv['LIBS'] += ['opengl32']
+				else :
+					lenv['LIBS'] += ['GL']
+
+			### configure openIL ###
+			elif elt == 'openil' :
+				self.use_openIL( lenv, elt )
+
+			### configure openILU ###
+			elif elt == 'openilu' :
+				self.use_openILU( lenv, elt )
+
 			### configure sdl ###
-			elif ( elt == 'sdl' ) :
+			elif elt == 'sdl' :
 				self.use_sdl( lenv, elt )
 
 			### configure sofa ###
-			elif ( elt == 'sofa' ) :
+			elif elt == 'sofa' :
 				self.use_sofa( lenv, elt )
 
-			### configure wx* ###
-			elif (	re.match('^wx[\d-]*$', elt ) or
-					re.match('^wxgl[\d-]*$', elt ) or
-					elt in ['wx', 'wxgl'] ) :
+			### configure wxWidgets ###
+			elif elt in ['wx2-8', 'wxgl2-8'] :
 				self.use_wxWidgets( lenv, elt )
 
 			### configure others ? ###
 			else :
-				print "sbfWarning: unknown uses=['%s']" % elt
+				raise SCons.Errors.UserError("Unknown uses=['%s']" % elt)
+
 
 
 	def vcsCheckout( self ) :
