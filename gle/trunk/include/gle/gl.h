c5a9e368d2d081c13e1f5d9b5596145b24368e4b
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2009-04-09 12:43:32 +0000

    + Adds gl.h in gle include directory to ease gle usage by simply including this file from others projects.

---
+++ gle/trunk/include/gle/gl.h
@@ -0,0 +1,13 @@
+// GLE - Copyright (C) 2009, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifndef _GLE_GL_H
+#define _GLE_GL_H
+
+#include "gle/gle.hpp"
+#include "gle/Wrapper.hpp"
+#include "gle/WrapperGen.hpp"
+
+#endif //#ifndef _GLE_GL_H
