84ab4730053cca9490a32a3fd893addb86ad9123
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2010-03-04 11:13:27 +0000

    ~ Removes an assertion in GLSLProgram::getUniformLocation(). ~ Improves outputs produced by compilation and link of GLSL shaders.

--- glo/trunk/src/GLSLProgram.cpp
+++ glo/trunk/src/GLSLProgram.cpp
@@ -1,4 +1,4 @@
-// GLE - Copyright (C) 2005, 2007, 2008, Nicolas Papier.
+// GLE - Copyright (C) 2005, 2007, 2008, 2010, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -52,7 +52,7 @@ GLSLProgram::GLSLProgram( bool initialized )
 
 	if ( initialized )
 	{
-		m_programObject = glCreateProgramObjectARB();
+		m_programObject = glCreateProgram();
 	}
 	else
 	{
@@ -80,13 +80,13 @@ void GLSLProgram::release()
 {
 	if ( m_programObject != 0 && gleGetCurrent() )
 	{
-		glDeleteObjectARB( m_programObject );
+		glDeleteProgram( m_programObject );
 	}
 }
 
 
 
-const bool GLSLProgram::addShader( const GLcharARB *shaderSource, const ShaderType shaderType, const bool linkProgram )
+const bool GLSLProgram::addShader( const GLchar *shaderSource, const ShaderType shaderType, const bool linkProgram )
 {
 	assert( m_programObject	!= 0	);
 
@@ -95,21 +95,30 @@ const bool GLSLProgram::addShader( const GLcharARB *shaderSource, const ShaderTy
 	// SET SOURCES
 	// @todo Creates a GLSLShader.[shaderSource, compile, compileStatus, infoLog, attachTo(GLSLProgram, deleteShader)]
 	// @todo exceptions
-	GLhandleARB object = glCreateShaderObjectARB( convertShaderType2GLEnum(shaderType) );
+	GLhandleARB object = glCreateShader( convertShaderType2GLEnum(shaderType) );
 	assert(object != 0);
 
 	const GLint length = static_cast<GLint>( std::strlen( shaderSource ) );
-	glShaderSourceARB( object, 1, &shaderSource, &length );
+	glShaderSource( object, 1, &shaderSource, &length );
 
 	// COMPILE shader object
-	glCompileShaderARB( object );
+	glCompileShader( object );
 
 	//CHECK if shader compiled
 	GLint compiled = 0;
 	glGetObjectParameterivARB( object, GL_OBJECT_COMPILE_STATUS_ARB, &compiled );
 
 //#ifdef _DEBUG
-	std::cout << convertShaderType2String(shaderType) << " shader compilation" << std::endl;
+	if ( compiled )
+	{
+		std::cout << convertShaderType2String(shaderType) << " shader have been successfully compiled." << std::endl;
+	}
+	else
+	{
+		std::cout << convertShaderType2String(shaderType) << " shader failed to compile." << std::endl;
+	}
+
+	std::cout << convertShaderType2String(shaderType) << " shader compilation log :" << std::endl;
 	printInfoLog( object ); // FIXME
 //#endif
 
@@ -121,10 +130,10 @@ const bool GLSLProgram::addShader( const GLcharARB *shaderSource, const ShaderTy
 	}
 
 	// ATTACH shader to program object
-	glAttachObjectARB( m_programObject, object );
+	glAttachShader( m_programObject, object );
 
 	// DELETE object, no longer needed
-	glDeleteObjectARB( object );
+	glDeleteShader( object );
 
 	// LINK stage
 	if ( linkProgram )
@@ -149,27 +158,28 @@ const bool GLSLProgram::link()
 	assert( getProgramObject() != 0 && "Empty glsl program" );
 
 	// Link program
-	glLinkProgramARB( getProgramObject() );
+	glLinkProgram( getProgramObject() );
 
 	// Checks status
 	GLint linked;
 	glGetObjectParameterivARB( getProgramObject(), GL_OBJECT_LINK_STATUS_ARB, &linked );
 
-//#ifdef _DEBUG
-	std::cout << "Program link" << std::endl;
-	printInfoLog( getProgramObject() );
-//#endif
-
 	if ( !linked )
 	{
-		std::cerr << "Program failed to link..." << std::endl;
+		std::cout << "PROGRAM failed to link..." << std::endl;
+		std::cerr << "PROGRAM failed to link..." << std::endl;
 		//printInfoLog( getProgramObject() );
 	}
 	else
 	{
-		std::cout << "Program have been successfully linked." << std::endl;
+		std::cout << "PROGRAM have been successfully linked." << std::endl;
 		//printInfoLog( getProgramObject() );
 	}
+
+//#ifdef _DEBUG
+	std::cout << "PROGRAM link log :" << std::endl;
+	printInfoLog( getProgramObject() );
+//#endif
 
 	return linked;
 }
@@ -187,7 +197,6 @@ void GLSLProgram::use()
 const bool GLSLProgram::isInUse() const
 {
 	GLint currentProgram;
-
 	glGetIntegerv( GL_CURRENT_PROGRAM, &currentProgram );
 
 	return currentProgram == getProgramObject();
@@ -206,28 +215,28 @@ void GLSLProgram::setUniform1i( const std::string & name, const GLint v1 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform1iARB( loc, v1 );
+	glUniform1i( loc, v1 );
 }
 
 void GLSLProgram::setUniform2i( const std::string & name, const GLint v1, const GLint v2 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform2iARB( loc, v1, v2 );
+	glUniform2i( loc, v1, v2 );
 }
 
 void GLSLProgram::setUniform3i( const std::string & name, const GLint v1, const GLint v2, const GLint v3 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform3iARB( loc, v1, v2, v3 );
+	glUniform3i( loc, v1, v2, v3 );
 }
 
 void GLSLProgram::setUniform4i( const std::string & name, const GLint v1, const GLint v2, const GLint v3, const GLint v4 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform4iARB( loc, v1, v2, v3, v4 );
+	glUniform4i( loc, v1, v2, v3, v4 );
 }
 
 
@@ -236,28 +245,28 @@ void GLSLProgram::setUniform1f( const std::string & name, const GLfloat v1 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform1fARB( loc, v1 );
+	glUniform1f( loc, v1 );
 }
 
 void GLSLProgram::setUniform2f( const std::string & name, const GLfloat v1, const GLfloat v2 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform2fARB( loc, v1, v2 );
+	glUniform2f( loc, v1, v2 );
 }
 
 void GLSLProgram::setUniform3f( const std::string & name, const GLfloat v1, const GLfloat v2, const GLfloat v3 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform3fARB( loc, v1, v2, v3 );
+	glUniform3f( loc, v1, v2, v3 );
 }
 
 void GLSLProgram::setUniform4f( const std::string & name, const GLfloat v1, const GLfloat v2, const GLfloat v3, const GLfloat v4 )
 {
 	const GLint loc = getUniformLocation( name );
 
-	glUniform4fARB( loc, v1, v2, v3, v4 );
+	glUniform4f( loc, v1, v2, v3, v4 );
 }
 
 
@@ -462,9 +471,9 @@ GLhandleARB GLSLProgram::getProgramObject() const
 
 const GLint GLSLProgram::getUniformLocation( const std::string& name )
 {
-	const GLint location = glGetUniformLocationARB( getProgramObject(), name.c_str() );
+	const GLint location = glGetUniformLocation( getProgramObject(), name.c_str() );
 
-	assert( location != -1 && "Name does not correspond to an active uniform" );
+	// assert( location != -1 && "Name does not correspond to an active uniform" );
 #ifdef _DEBUG
 	if ( location == -1 )
 	{
@@ -527,7 +536,7 @@ GLenum GLSLProgram::m_GLEnumShaderType[] =
 {
 	GL_VERTEX_SHADER_ARB,
 	GL_FRAGMENT_SHADER_ARB,
-	GL_GEOMETRY_SHADER_EXT
+	GL_GEOMETRY_SHADER_ARB
 };
 
 
