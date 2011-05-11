5448ded4702da0cc1f68b933850d6521751b119e
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2011-05-11 13:14:16 +0000

    + Adds support for (V)ertex (A)rray (O)bject.

---
+++ glo/trunk/include/glo/VertexArrayObject.hpp
@@ -0,0 +1,115 @@
+// OGLPP - Copyright (C) 2011, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#ifndef _GLO_VERTEXARRAYOBJECT_HPP
+#define _GLO_VERTEXARRAYOBJECT_HPP
+
+#include "glo/Object.hpp"
+
+
+
+namespace glo
+{
+
+/**
+ * @brief (V)ertex (A)rray (O)bject
+ * 
+ * This class is an encapsulation of a single VAO. At OpenGL level, all state related to the definition of data
+ * used by the vertex processor is encapsulated in a VAO.
+ */
+struct VertexArrayObject : public Object
+{
+	/**
+	 * @name Constructor/destructor
+	 */
+	//@{
+	
+	/**
+	 * @brief Default constructor
+	 * 
+	 * The underlying OpenGL object is NOT generated.
+	 */
+	GLO_API VertexArrayObject();
+	
+	/**
+	 * @brief Destructor
+	 */
+	GLO_API ~VertexArrayObject();
+
+	//@}
+
+
+
+	/**
+	 * @name Actions
+	 */
+	//@{
+	
+	/**
+	 * @brief Generates the vertex array object
+	 * 
+	 * @pre isEmpty() must return true
+	 * @post isEmpty() must return false
+	 */
+	GLO_API void generate();
+
+	/**
+	 * @brief Deletes the vertex array object
+	 * 
+	 * @post isEmpty() must return false
+	 */
+	GLO_API void release();
+
+	/**
+	 * @brief Binds the vertex array object
+	 * 
+	 * @pre isEmpty() must return false
+	 */
+	GLO_API void bind() const;
+
+	/**
+	 * @brief Unbinds the vertex array object
+	 * 
+	 * @pre isEmpty() must return false
+	 * @pre isBound() must return true
+	 */
+	GLO_API void unbind() const;
+
+	//@}
+
+
+
+	/**
+	 * @name Accessors
+	 */
+	//@{
+
+	/**
+	 * @brief Returns if the vertex array object is bounded
+	 * 
+	 * @pre isEmpty() must return false
+	 * 
+	 * @return true if the vertex array object is bounded, false otherwise.
+	 */
+	GLO_API const bool isBound() const;
+	//@}
+
+
+
+
+
+
+	/**
+	 * @name Queries
+	 */
+	//@{
+	//@}
+};
+
+
+
+} // namespace glo
+
+#endif //#ifndef _GLO_VERTEXARRAYOBJECT_HPP
---
+++ glo/trunk/src/VertexArrayObject.cpp
@@ -0,0 +1,94 @@
+// OGLPP - Copyright (C) 2011, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include "glo/VertexArrayObject.hpp"
+
+#include <iostream>
+
+
+
+namespace glo
+{
+
+
+
+VertexArrayObject::VertexArrayObject()
+{}
+
+
+
+VertexArrayObject::~VertexArrayObject()
+{
+	if ( gleGetCurrent() )
+	{
+		release();
+	}
+	else
+	{
+		std::cerr << "Unable to release vertex array object " << m_object << "." << std::endl;
+	}
+}
+
+
+
+void VertexArrayObject::generate()
+{
+	assert( isEmpty() );
+
+	glGenVertexArrays(1, &m_object);
+
+	assert( !isEmpty() );
+}
+
+
+
+void VertexArrayObject::release()
+{
+	if ( !isEmpty() )
+	{
+		bind();
+
+		glDeleteVertexArrays(1, &m_object);
+
+		m_object = 0;
+	}
+
+	assert( isEmpty() );
+}
+
+
+
+void VertexArrayObject::bind() const
+{
+	assert( !isEmpty() );
+
+	glBindVertexArray( m_object );
+}
+
+
+
+void VertexArrayObject::unbind() const
+{
+	assert( !isEmpty() );
+	assert( isBound() );
+
+	glBindVertexArray( 0 );
+}
+
+
+
+const bool VertexArrayObject::isBound() const
+{
+	assert( !isEmpty() );
+
+	GLint currentBinding;
+	glGetIntegerv( GL_VERTEX_ARRAY_BINDING, &currentBinding );
+
+	return ( static_cast<GLuint>(currentBinding) == m_object );
+}
+
+
+
+} // namespace glo
