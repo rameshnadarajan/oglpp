0e306f13b8a8ee0996e04c48dd9e51770b9c3f0c
guillaume.brocker <guillaume.brocker@af2efa36-d00a-11dd-911d-6bf5514d214c>
2014-06-17 10:57:28 +0000

    ~ Adds support of glx.

---
+++ glContext/trunk/glc/src/types.cpp
@@ -0,0 +1,6 @@
+// OGLPP - Copyright (C) 2008, 2009, 2010, 2011, 2013, 2014, Nicolas Papier.
+// Distributed under the terms of the GNU Library General Public License (LGPL)
+// as published by the Free Software Foundation.
+// Author Nicolas Papier
+
+#include "glc/types.hpp"
