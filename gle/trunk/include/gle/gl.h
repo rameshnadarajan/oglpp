6ec1a3b4fc31b8c2ba3e5585affdd9db06f67ea3
npapier <npapier@af2efa36-d00a-11dd-911d-6bf5514d214c>
2014-11-21 08:34:49 +0000

    ~ Disables gle when used with emscripten or OpenGL ES 2.

--- gle/trunk/include/gle/gl.h
+++ gle/trunk/include/gle/gl.h
@@ -1,4 +1,4 @@
-// GLE - Copyright (C) 2009, Nicolas Papier.
+// GLE - Copyright (C) 2009, 2014, Nicolas Papier.
 // Distributed under the terms of the GNU Library General Public License (LGPL)
 // as published by the Free Software Foundation.
 // Author Nicolas Papier
@@ -6,9 +6,18 @@
 #ifndef _GLE_GL_H
 #define _GLE_GL_H
 
-#include "gle/gle.hpp"
-#include "gle/glHelpers.hpp"
-#include "gle/Wrapper.hpp"		// for gleSetCurrent(), gleGetCurrent()
-#include "gle/WrapperGen.hpp"
+// See glo/glo.hpp and vgeGL/vgeGL.hpp
+#if defined(__OPENGLES2__) || defined(__EMSCRIPTEN__)
+	#error "Try to use gle with OpenGLES2 or emscripten. Not yet allowed."
+	// emscripten => WebGL 1.0 (i.e. almost OpenGLES2.0)
+	#include <GLES2/gl2.h>
+	#include <GLES2/gl2ext.h>
+#else
+	// Desktop OpenGL (3.x and 4.x)
+	#include "gle/gle.hpp"
+	#include "gle/glHelpers.hpp"
+	#include "gle/Wrapper.hpp"		// for gleSetCurrent(), gleGetCurrent()
+	#include "gle/WrapperGen.hpp"
+#endif
 
 #endif //#ifndef _GLE_GL_H
